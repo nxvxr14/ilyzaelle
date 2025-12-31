5ec16ea2c2dd88ac9b0b72cbd45e552462fefdd1
nxvxr14 <hubertdelgado14@gmail.com>
2025-12-31 10:37:12 -0500

    Actualiza logo de la UNAB en el frontend

--- frontend_react/src/components/LogoUNAB.tsx
+++ frontend_react/src/components/LogoUNAB.tsx
@@ -1,10 +1,16 @@
 function LogoUNAB() {
   return (
     <div className="flex items-center justify-center">
-      <img 
+      {/* <img 
         src="https://fundacompucesco.org.co/wp-content/uploads/2022/03/logo_unabvirtual.png" 
         alt="Logo UNAB" 
         className="m-5 w-[120px] h-auto"  // Ajusta el tamaño con Tailwind
+      /> */}
+
+      <img
+        src="https://i.postimg.cc/Pxywt6Zz/New-Project.png"
+        alt="Logo UNAB"
+        className="m-5 w-[300px] h-auto"  // Ajusta el tamaño con Tailwind
       />
       {/* <span className="ml-4 text-xl font-semibold">Ilyzaelle</span> */}
     </div>
