c7443586b786c9ad519dd98e0b58e88a1b8b7f22
nxvxr14 <hubertdelgado14@gmail.com>
2026-02-10 13:28:09 -0500

    new start, ahora si xd

--- labsFrontend/src/api/leaderboardApi.ts
+++
@@ -1,7 +0,0 @@
-import api from './axios';
-import type { ApiResponse, LeaderboardEntry } from '@/types';
-
-export const getLeaderboard = async (): Promise<ApiResponse<LeaderboardEntry[]>> => {
-  const { data } = await api.get<ApiResponse<LeaderboardEntry[]>>('/leaderboard');
-  return data;
-};
